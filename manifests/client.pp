d73b713a9d195dc82885af52e6e054b3db6a1313
OpCode1300 <alex@opcode1300.com>
2014-08-20 15:54:01 -0400

    2014.1 files
    updated links to 2014.1

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,7 +1,7 @@
 class perforce::client() {
 
   wget::fetch { 'p4':
-    source      => 'http://www.perforce.com/downloads/perforce/r12.1/bin.linux26x86_64/p4',
+    source      => 'http://cdist2.perforce.com/perforce/r14.1/bin.linux26x86_64/p4',
     destination => '/usr/bin',
   } ->
   file { '/usr/bin/p4':
