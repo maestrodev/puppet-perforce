ae244d1ef768ee7646a34414e9dc5e800b544929
pulquero <mj_hale@yahoo.com>
2015-06-11 16:23:11 +0100

    Added support to specify version

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,7 +1,9 @@
-class perforce::client() {
+class perforce::client(
+  $version = '15.1'
+) {
 
   wget::fetch { 'p4':
-    source      => 'http://cdist2.perforce.com/perforce/r14.1/bin.linux26x86_64/p4',
+    source      => "http://cdist2.perforce.com/perforce/r${version}/bin.linux26x86_64/p4",
     destination => '/usr/bin/p4',
   } ->
   file { '/usr/bin/p4':
