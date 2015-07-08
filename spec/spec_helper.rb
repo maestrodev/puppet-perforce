b017e78a85819cea8accb71778cde9fa8831f8e0
Carlos Sanchez <carlos@apache.org>
2015-07-08 22:40:14 +0200

    Run modulesync

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,33 @@
-require 'rspec-puppet'
+# This file is managed centrally by modulesync
+#   https://github.com/maestrodev/puppet-modulesync
 
-fixture_path = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures'))
+require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |c|
-  c.module_path = File.join(fixture_path, 'modules')
-  c.manifest_dir = File.join(fixture_path, 'manifests')
+  c.mock_with :rspec
+  c.hiera_config = File.expand_path(File.join(__FILE__, '../fixtures/hiera.yaml'))
+
+  c.before(:each) do
+    Puppet::Util::Log.level = :warning
+    Puppet::Util::Log.newdestination(:console)
+  end
+
+  c.default_facts = {
+    :operatingsystem => 'CentOS',
+    :operatingsystemrelease => '6.6',
+    :kernel => 'Linux',
+    :osfamily => 'RedHat',
+    :architecture => 'x86_64',
+    :clientcert => 'puppet.acme.com'
+  }.merge({})
+
+  c.before do
+    # avoid "Only root can execute commands as other users"
+    Puppet.features.stubs(:root? => true)
+  end
 end
+
+shared_examples :compile, :compile => true do
+  it { should compile.with_all_deps }
+end
+
